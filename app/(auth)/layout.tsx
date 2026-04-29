bd345d84250df01914a625147a71702ac0470bb9
cgtrepo <christallohan@gmail.com>
2026-04-29 16:19:18 +0100

    fix: type children in layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,9 @@
-export default function AuthLayout({ children }) {
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function AuthLayout({ children }: Props) {
   return <div className="min-h-screen w-full">{children}</div>;
 }
